02c6c98035e3c3465a815f6fc0c83f6ae72d9da5
Katasandi11 <firmanmuhksin@gmail.com>
2024-11-08 15:37:10 +0700

    first commit

---
+++ app/Filament/Resources/BarangResource.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BarangResource\Pages;
+use App\Filament\Resources\BarangResource\RelationManagers;
+use App\Models\Barang;
+use Filament\Forms;
+use Filament\Forms\Form;
+
+use Filament\Forms\Components\TextInput; 
+use Filament\Forms\Components\Textarea; 
+
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+
+
+
+
+class BarangResource extends Resource
+{
+    protected static ?string $model = Barang::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+        ->schema([ 
+            TextInput::make('nama') 
+            ->required() 
+            ->label('Nama Barang'), 
+            TextArea::make('deskripsi') 
+            ->label('Deskripsi'), 
+            TextInput::make('harga') 
+            ->numeric() 
+            ->required() 
+            ->label('Harga'), ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('nama'),
+                TextColumn::make('deskripsi'),
+                TextColumn::make('harga'),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBarangs::route('/'),
+            'create' => Pages\CreateBarang::route('/create'),
+            'edit' => Pages\EditBarang::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
---
+++ app/Filament/Resources/BarangResource/Pages/EditBarang.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\BarangResource\Pages;
+
+use App\Filament\Resources\BarangResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditBarang extends EditRecord
+{
+    protected static string $resource = BarangResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+            Actions\ForceDeleteAction::make(),
+            Actions\RestoreAction::make(),
+        ];
+    }
+}
